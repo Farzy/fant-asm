9cd4656cc63f2c5bd04b0158b51405f631966ba0
Farzad FARID <farzad.farid@kapten.com>
2020-08-28 18:03:33 +0200

    Test variables

--- hello_macos.asm
+++ hello_macos.asm
@@ -17,10 +17,31 @@ _main:    mov       rax, 0x02000004         ; system call for write
           lea       rsi, [message]          ; address of string to output
           mov       rdx, message.len        ; number of bytes
           syscall                           ; invoke operating system to do the write
+
+          mov       rax, 0x02000004         ; system call for write
+          mov       rdi, 1                  ; file handle 1 is stdout
+          lea       rsi, [var1]          ; address of string to output
+          mov       rdx, var1Len        ; number of bytes
+          syscall                           ; invoke operating system to do the write
+
+          mov       rax, 0x02000004         ; system call for write
+          mov       rdi, 1                  ; file handle 1 is stdout
+          lea       rsi, [var2]          ; address of string to output
+          mov       rdx, var2Len        ; number of bytes
+          syscall                           ; invoke operating system to do the write
+
           mov       rax, 0x02000001         ; system call for exit
           xor       rdi, rdi                ; exit code 0
           syscall                           ; invoke operating system to exit
 
           section   .data
+
 message:  db        "Hello, World", 10      ; note the newline at the end
 .len      equ       $ - message
+
+var1:       db      "123", 10
+var1Len     equ     $ - var1
+
+var2:       TIMES 5 db      "987€ "
+            db      10
+var2Len     equ     $ - var2
