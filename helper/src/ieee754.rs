1e41e49c5194338f1dea280d3349b8adb25a3350
Farzad FARID <farzy@farzy.org>
2020-08-31 21:56:04 +0200

    Refactor command line parsing and use hex number from args

--- helper/src/ieee754.rs
+++ helper/src/ieee754.rs
@@ -1,14 +1,12 @@
-pub fn main() {
-    let x: u32 = 0xC1440000;
+pub fn main(hex: u32) {
+    println!("{:08X} = {:032b}", hex, hex);
 
-    println!("{:08X} = {:032b}", x, x);
-
-    let sign = x >> 31; // 1 bit
-    let exponent = ((x & 0x7FFFFFFF) >> 23) - 127; // 8 bits
-    let fraction = x & 0b01111111_11111111_11111111; // 23 bits
+    let sign = (hex >> 31) as u8; // 1 bit
+    let exponent = (((hex & 0x7FFFFFFF) >> 23) as i16 - 127) as i8; // 8 bits
+    let fraction = hex & 0b01111111_11111111_11111111; // 23 bits
 
     println!(
-        "Sign: {:0}, exponent: {}, fraction = 1.{:023b}",
+        "Sign: {:0}, exponent: {}, fraction = 0b1.{:023b}",
         sign, exponent, fraction
     );
 }
--- helper/src/main.rs
+++ helper/src/main.rs
@@ -1,4 +1,4 @@
-use clap::{App, Arg};
+use clap::{App, Arg, SubCommand};
 
 mod ieee754;
 
@@ -7,14 +7,31 @@ fn main() {
         .about("Helper functions for learning assembly")
         .version("1.0")
         .author("Farzad FARID <farzy@farzy.org>")
-        .arg(
-            Arg::with_name("ieee754")
-                .help("Floating point conversion")
-                .long("ieee754"),
+        .subcommand(
+            SubCommand::with_name("ieee754")
+                .about("Floating point conversion")
+                .arg(
+                    Arg::with_name("HEX")
+                        .help("Hexadecimal representation of an IEEE 754 floating point")
+                        .required(true)
+                        .index(1),
+                ),
         )
         .get_matches();
 
-    if matches.is_present("ieee754") {
-        ieee754::main();
+    if let Some(matches) = matches.subcommand_matches("ieee754") {
+        // println!("Using HEX: {}", matches.value_of("HEX").unwrap());
+        let mut s = matches.value_of("HEX").unwrap();
+        if &s[0..2] == "0x" || &s[0..2] == "0X" {
+            s = &s[2..];
+        }
+        let hex = match u32::from_str_radix(s, 16) {
+            Ok(num) => num,
+            Err(e) => {
+                eprintln!("Error parsing hexadecimal number '{}: {}", s, e);
+                std::process::exit(1);
+            }
+        };
+        ieee754::main(hex);
     }
 }
