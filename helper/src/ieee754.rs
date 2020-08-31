58f695242ac1d4aaf4af68e26f0ac68500b06f00
Farzad FARID <farzy@farzy.org>
2020-08-31 23:18:26 +0200

    Improve sign display

--- helper/src/ieee754.rs
+++ helper/src/ieee754.rs
@@ -1,12 +1,12 @@
 pub fn main(hex: u32) {
     println!("{:08X} = {:032b}", hex, hex);
 
-    let sign = (hex >> 31) as u8; // 1 bit
+    let sign = if (hex >> 31) == 0 { 1i8 } else { -1i8 };
     let exponent = (((hex & 0x7FFFFFFF) >> 23) as i16 - 127) as i8; // 8 bits
     let fraction = hex & 0b01111111_11111111_11111111; // 23 bits
 
     println!(
-        "Sign: {:0}, exponent: {}, fraction = 0b1.{:023b}",
+        "Sign: {}, exponent: {}, fraction = 0b1.{:023b}",
         sign, exponent, fraction
     );
 }
